be1fec4c1fe1f70ac29df62cd03dc3cf0c8d34db
CadenceReynolds <reynocal001@warren.k12.in.us>
2025-01-16 13:49:46 -0500

    Finished assignment

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        Box<String> stringBox = new Box<>();
+        Box integerBox = new Box();
+        integerBox.setItem("String");
+        integerBox.setItem(3);
+        System.out.println(integerBox.getItem());
+    }
+
 }
